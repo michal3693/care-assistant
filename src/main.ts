63779f0639f8a5b17ca3e5fa27f3055be52e00e7
Michał Gondek <michalgondek.contact@gmail.com>
2024-05-30 11:37:19 +0200

    add: firebase

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 if (environment.production) {
   enableProdMode();
@@ -15,6 +18,6 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes),
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"care-assistant-caa3f","appId":"1:835973821037:web:c36eb4e2a58759171dc564","storageBucket":"care-assistant-caa3f.appspot.com","apiKey":"AIzaSyB_ny7bqf0dQz8qifB_6Q9Pc-GPz909xWI","authDomain":"care-assistant-caa3f.firebaseapp.com","messagingSenderId":"835973821037","measurementId":"G-LND2G64LH1"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 });
